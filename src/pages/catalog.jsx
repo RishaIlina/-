dd7ffbfef48facc9251edbac439e47dff00ef136
Ирина <brulichka@bk.ru>
2024-02-15 20:49:22 +0400

    Стилизация корзины товаров

--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -4,6 +4,22 @@ import Image from "next/image";
 export default function catalog() {
   return (
     <section className="products">
+
+        <div className={styles.products__overlay}>
+            <div className={styles.products__drawer}>
+                <h2 className={styles.products__drawer_title}>Корзина</h2>
+                <div className={styles.cart__item}>
+                    <Image src="/image/shopping__card-candle-1.jpg" alt="Свеча" width={80} height={80} className={styles.cart__item_img} />
+                    <div className={styles.cart__item_descr}>
+                        <p className={styles.cart__item_title}>Свеча "Ранункулюс"</p>
+                        <p className={styles.cart__item_price}>820₽</p>
+                    </div>
+                    <Image src="/image/btn-remove.svg" alt="Кнопка закрытия корзины" width={31} height={31} className={styles.cart__item_remove} />
+                </div>
+            </div>
+        </div>
+        
+
       <div className={styles.products__search}>
         <div className={styles.search__block}>
           <Image
