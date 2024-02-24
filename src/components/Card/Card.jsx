70d35d73d5a49fd7a1ff0923530c73ea149f3b65
Ирина <brulichka@bk.ru>
2024-02-24 14:07:24 +0400

    Поправила структуру каталога

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,62 +5,21 @@ import { useState, useEffect } from "react";
 /**
  * Компонент карточки товара.
  */
-export default function Card() {
-  // Стейт для отображения карточек
-  const [products, setProducts] = useState([]);
+export default function Card(props) {
+  const { name, imgSrc, price } = props.details;
 
-  useEffect(() => {
-    getProductsFromServer(); // Вызов для отрисовки данных
-  }, []);
-
-  /// Объект состояний для информации о добавлении товаров в корзину
-  const [isAddedState, setIsAddedState] = useState({});
-
-  /**
-   * Функция для получения товаров из базы данных.
-   */
-  const getProductsFromServer = () => {
-    fetch(`https://65d386d8522627d501091517.mockapi.io/products`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Ошибка при получении данных");
-        }
-        return response.json();
-      })
-      .then((data) => setProducts(data))
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-   /**
-   * Функция для обновления состояния конкретной карточки.
-   * @param {number} id - Идентификатор товара.
-   */
-  const onClickAddToCart = (id) => {
-    setIsAddedState(prevState => ({
-      ...prevState,
-      [id]: !prevState[id]
-    }));
-  };
-
-  // Итерация по данным и отрисовка карточек
-  const renderData =
-    products.length > 0 &&
-    products.map((product) => (
-      <div
-        key={product.id}
-        className={` products__item ${styles.products__item_card} `}
-      >
+  return (
+    <>
+      <div className={` products__item ${styles.products__item_card} `}>
         <Image
           width={174}
           height={210}
-          src={product?.imgSrc}
-          alt={product?.name}
+          src={imgSrc}
+          alt={name}
           className={styles.products__item_img}
         />
         <div className={styles.products__item_top}>
-          <p>{product?.name}</p>
+          <p>{name}</p>
           <div className={styles.favorite}>
             <Image
               width={17}
@@ -73,26 +32,25 @@ export default function Card() {
         <div className={styles.products__item_bottom}>
           <div className={styles.products__item_info}>
             <span>Цена:</span>
-            <p className={styles.products__item_price}>{product?.price}</p>
+            <p className={styles.products__item_price}>{price}</p>
           </div>
           <button
             className="btn_icon"
-            onClick={() => onClickAddToCart(product.id)}
+            /*onClick={() => onClickAddToCart(product.id)}*/
           >
             <Image
               width={32}
               height={32}
               src={
-                isAddedState[product.id]
+               /* isAddedState[product.id]
                   ? "/image/btn-checked.svg"
-                  : "/image/btn-plus.svg"
+                  :*/ "/image/btn-plus.svg"
               }
               alt="Добавить"
             />
           </button>
         </div>
       </div>
-    ));
-
-  return <div className={`container products_inner`}>{renderData}</div>;
+    </>
+  );
 }
--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -1,15 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "../components/Card/Card";
 import styles from "../styles/Catalog.module.css";
 import Image from "next/image";
 
 export default function Catalog() {
-  // стейт для хранения товаров в корзине
-  const [cartProducts, setCartProducts] = useState([]);
+  // Стейт для отображения карточек
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProductsFromServer(); // Вызов для отрисовки данных
+  }, []);
+
+  /**
+   * Функция для получения товаров из базы данных.
+   */
+  const getProductsFromServer = () => {
+    fetch("https://65d386d8522627d501091517.mockapi.io/products")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Ошибка при получении данных");
+        }
+        return response.json();
+      })
+      .then((data) => setProducts(data))
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  // итерация по данным и отрисовка карточек
+  const productList =
+  products.length > 0 &&
+  products.map((product) => (
+      <Card key={product?.id} details={product} />
+    ));
 
   return (
     <section className="products">
-
       <div className={styles.products__search}>
         <div className={styles.search__block}>
           <Image
@@ -24,7 +51,7 @@ export default function Catalog() {
       </div>
 
       <div className={`container products_inner`}>
-        <Card />
+        {productList}
       </div>
     </section>
   );
