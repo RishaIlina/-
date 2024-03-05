f61243214197af8756c157d0b56dfdfefa964dba
Ирина <brulichka@bk.ru>
2024-03-05 21:31:13 +0400

    Сделала модальное окно входа в систему

--- src/components/ui/RegistrationForm/RegistrationForm.jsx
+++ src/components/ui/RegistrationForm/RegistrationForm.jsx
@@ -1,121 +1,47 @@
 import { useState } from "react";
-import {
-  FaGooglePlusG,
-  FaFacebookF,
-  FaGithub,
-  FaLinkedinIn,
-} from "react-icons/fa";
 import styles from "./RegistrationForm.module.css";
+import Image from "next/image";
 
-export default function SigninSignupPage({ isOpen }) {
-  const [isSignUpActive, setIsSignUpActive] = useState(false);
-  const [isSignInActive, setIsSignInActive] = useState(false);
+export default function RegistrationForm({ isOpen }) {
+  const [action, setAction] = useState("Регистрация");
 
-  // Обработчик события для кнопки "Sign Up"
-  const handleSignUpClick = (e) => {
-    e.preventDefault();
-    setIsSignUpActive(true);
-    setIsSignInActive(false);
-    console.log("Sign Up Active State:", isSignUpActive);
-  };
-
-  // Обработчик события для кнопки "Sign In"
-  const handleSignInClick = (e) => {
-    e.preventDefault();
-    setIsSignInActive(true);
-    setIsSignUpActive(false);
-    console.log("Sign In Active State:", isSignInActive);
-  };
 
   return (
     isOpen && (
       <div className={styles.container}>
-        <div
-          className={`${styles.form_container} ${
-            isSignUpActive ? styles.sign_up : ""
-          }`}
-        >
-          <form>
-            <h1 className={styles.modal_title}>Cоздать аккаунт</h1>
-            <div className={styles.social_icons}>
-              <a href="#" className={styles.icon}>
-                <FaGooglePlusG />
-              </a>
-              <a href="#" className={styles.icon}>
-                <FaFacebookF />
-              </a>
-              <a href="#" className={styles.icon}>
-                <FaGithub />
-              </a>
-              <a href="#" className={styles.icon}>
-                <FaLinkedinIn />
-              </a>
-            </div>
-            <span>или используйте свой адрес электронной почты для регистрации</span>
+        <div className={styles.header}>
+          <div className={styles.text}>{action}</div>
+          <div className={styles.underline}></div>
+        </div>
+        <div className={styles.inputs}>
+          <div className={styles.input}>
+            <Image src="/image/person.png" alt="пользователь" width={15} height={15} />
             <input type="text" placeholder="Имя" />
-            <input type="email" placeholder="Почта" />
-            <input type="password" placeholder="Пароль" />
-            <button
-              onClick={handleSignUpClick}
-              type="button"
-            >
-              Зарегистрироваться
-            </button>
-          </form>
-        </div>
-        <div
-          className={`${styles.form_container} ${
-            isSignInActive ? styles.sign_in : ""
-          }`}
-        >
-          <form>
-            <h1>Вход</h1>
-            <div className={styles.social_icons}>
-              <a href="#" className={styles.icon}>
-                <FaGooglePlusG />
-              </a>
-              <a href="#" className={styles.icon}>
-                <FaFacebookF />
-              </a>
-              <a href="#" className={styles.icon}>
-                <FaGithub />
-              </a>
-              <a href="#" className={styles.icon}>
-                <FaLinkedinIn />
-              </a>
-            </div>
-            <span>или используйте пароль от электронной почты</span>
-            <input type="email" placeholder="Почта" />
-            <input type="password" placeholder="Пароль" />
-            <a href="#">Забыли свой пароль?</a>
-            <button
-              onClick={handleSignInClick}
-              type="button"
-            >
-              Войти
-            </button>
-          </form>
-        </div>
-        <div className={styles.toggle_container}>
-          <div className={styles.toggle}>
-            <div className={` ${styles.toggle_panel} ${styles.toggle_left} `}>
-              <h1>С возвращением!</h1>
-              <p>Введите свои личные данные, чтобы использовать все функции сайта</p>
-              <button type="button" className={styles.hidden}>
-                Войти
-              </button>
-            </div>
-            <div className={` ${styles.toggle_panel} ${styles.toggle_right} `}>
-              <h1>Привет, друг!</h1>
-              <p>
-              Зарегистрируйтесь, указав свои личные данные, чтобы использовать все функции сайта
-              </p>
-              <button type="button" className={styles.hidden}>
-                Регистрация
-              </button>
-            </div>
           </div>
-        </div>
+          <div className={styles.input}>
+            <Image src="/image/email.png" alt="почта" width={18} height={12} />
+            <input type="email" placeholder="Почта" />
+          </div>
+          <div className={styles.input}>
+            <Image src="/image/password.png" alt="пароль" width={15} height={15} />
+            <input type="password" placeholder="Пароль" />
+          </div>
+          <div className={styles.forgot_password}>Забыли пароль?<span>Нажмите здесь</span></div>
+          <div className={styles.submit_container}>
+           <div 
+        className={action === "Войти" ? `${styles.submit} ${styles.disabled}` : styles.submit} 
+        onClick={() => {setAction("Регистрация")}}
+      >
+        Зарегистрироваться
+      </div>
+      <div 
+        className={action === "Регистрация" ? `${styles.submit} ${styles.disabled}` : styles.submit} 
+        onClick={() => {setAction("Вход")}}
+      >
+        Войти
+      </div>
+          </div>
+        </div> 
       </div>
     )
   );
