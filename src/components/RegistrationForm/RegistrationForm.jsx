f4f32686c82e6fc095e24dc79ac679b1062118fa
Ирина <brulichka@bk.ru>
2024-03-17 16:10:38 +0400

    Добавила крестик для закрытия окна входа/регистрации

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -5,17 +5,36 @@ import Image from "next/image";
 export default function RegistrationForm({ isOpen }) {
   const [action, setAction] = useState("Регистрация");
 
+  // Закрытие модального окна
+  const closeRegistrationForm = () => {
+    isOpen(false);
+  };
 
   return (
     isOpen && (
       <div className={styles.container}>
         <div className={styles.header}>
-          <div className={styles.text}>{action}</div>
-          <div className={styles.underline}></div>
+          <div className={styles.text_decor}>
+            <div className={styles.text}>{action}</div>
+            <div className={styles.underline}></div>
+          </div>
+          <Image
+            src="/image/btn-remove.svg"
+            alt="Кнопка закрытия модального окна"
+            width={31}
+            height={31}
+            onClick={closeRegistrationForm}
+            className={styles.cart__item_close}
+          />
         </div>
         <div className={styles.inputs}>
           <div className={styles.input}>
-            <Image src="/image/person.png" alt="пользователь" width={15} height={15} />
+            <Image
+              src="/image/person.png"
+              alt="пользователь"
+              width={15}
+              height={15}
+            />
             <input type="text" placeholder="Имя" />
           </div>
           <div className={styles.input}>
@@ -23,25 +42,44 @@ export default function RegistrationForm({ isOpen }) {
             <input type="email" placeholder="Почта" />
           </div>
           <div className={styles.input}>
-            <Image src="/image/password.png" alt="пароль" width={15} height={15} />
+            <Image
+              src="/image/password.png"
+              alt="пароль"
+              width={15}
+              height={15}
+            />
             <input type="password" placeholder="Пароль" />
           </div>
-          <div className={styles.forgot_password}>Забыли пароль?<span>Нажмите здесь</span></div>
-          <div className={styles.submit_container}>
-           <div 
-        className={action === "Войти" ? `${styles.submit} ${styles.disabled}` : styles.submit} 
-        onClick={() => {setAction("Регистрация")}}
-      >
-        Зарегистрироваться
-      </div>
-      <div 
-        className={action === "Регистрация" ? `${styles.submit} ${styles.disabled}` : styles.submit} 
-        onClick={() => {setAction("Вход")}}
-      >
-        Войти
-      </div>
+          <div className={styles.forgot_password}>
+            Забыли пароль?<span>Нажмите здесь</span>
           </div>
-        </div> 
+          <div className={styles.submit_container}>
+            <div
+              className={
+                action === "Вход"
+                  ? `${styles.submit} ${styles.disabled}`
+                  : styles.submit
+              }
+              onClick={() => {
+                setAction("Регистрация");
+              }}
+            >
+              Зарегистрироваться
+            </div>
+            <div
+              className={
+                action === "Регистрация"
+                  ? `${styles.submit} ${styles.disabled}`
+                  : styles.submit
+              }
+              onClick={() => {
+                setAction("Вход");
+              }}
+            >
+              Войти
+            </div>
+          </div>
+        </div>
       </div>
     )
   );
