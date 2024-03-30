f8f19e1a598bf8f7d04aaa0a7eea635bde1ea53a
Ирина <brulichka@bk.ru>
2024-03-30 16:48:03 +0400

    Правка логики добавления товара в избранное.

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -3,11 +3,15 @@ import { CartContext } from "../components/CartContext/CartContext";
 import Card from "../components/Card/Card";
 import styles from "../styles/Favourites.module.css";
 
+/**
+ * Страница с избранными товарами.
+ */
 export default function Favourites() {
   const { products, favouriteItems } = useContext(CartContext);
 
   const [favouriteProducts, setFavouriteProducts] = useState([]);
 
+  // Обновляем список избранных товаров
   useEffect(() => {
     setFavouriteProducts(products.filter((product) => favouriteItems.includes(product.id)));
   }, [products, favouriteItems]);
